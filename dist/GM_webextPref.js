786179f85346b790dedb8ef7ba3b68829b385798
eight <eight04@gmail.com>
2018-08-19 23:46:10 +0800

    Release v0.1.2

--- dist/GM_webextPref.js
+++ dist/GM_webextPref.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name GM_webextPref
-// @version 0.1.1
+// @version 0.1.2
 // @description A config library powered by webext-pref.
 // @license MIT
 // @author eight04 <eight04@gmail.com>
